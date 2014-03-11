f45231319dad31b2937f883b5655157faa624c80
Eirik S. Morland <eirik@morland.no>
2014-03-11 22:20:49 +0100

    Make some more tests.

--- test/test.js
+++ test/test.js
@@ -1,10 +1,48 @@
-var oct = require('..');
+var oct = require('../src/app');
+oct.override({
+  loginurl: 'http://localhost:9876',
+  bookingurl: 'http://localhost:9876'
+});
+
 var should = require('should');
 
-describe('Test the module', function() {
-  it('Should not init without a config', function(done) {
+describe('Config', function() {
+
+  it('Should not init without a config', function() {
     var result = oct.init({});
     result.should.equal(false);
-    done();
+  });
+
+  it('Should init with a config', function() {
+    var result = oct.init({username: 'bogus'});
+    result.should.equal(true);
+  });
+});
+
+describe('Requests', function() {
+
+  it('Should try to go to first function in callback series, but fail', function(done) {
+    oct.start({username: 'bogus'}, function(err, res) {
+      err.should.be.instanceOf(Object);
+      done();
+    });
+  });
+
+  it('Should go all the way throught, if we start up a server', function(done) {
+    require('http').createServer(function (req, res) {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(JSON.stringify('Hello World\n'));
+    }).listen(9876, '127.0.0.1');
+    oct.start({
+      username: 'bogus',
+      dayOfWeek: 1,
+      times: [
+        '19:30',
+        '20:00'
+      ]
+    }, function(err, res) {
+      should(err).equal(null);
+      done();
+    });
   });
 });
